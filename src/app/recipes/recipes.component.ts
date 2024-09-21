ee2f445bb7f69f5d72e080a3f26a61a2a5d7b21d
Miguel Ángel <txrz56rad@live.com.mx>
2024-09-20 22:26:36 -0600

    Masonry layout for searched recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -4,13 +4,25 @@ import { RecipeService } from '../services/recipe.service';
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css',
+  styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent {
   private recipeService = inject(RecipeService);
-  recipes = computed(() => this.recipeService.recipes());
+  recipes = computed(() => this.recipeService.recipes() || []);
+
+  recipeChunks = computed(() => {
+    const allRecipes = this.recipes();
+    return this.chunkArray(allRecipes, 4);
+  });
+
+  chunkArray(arr: any[], chunkSize: number) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      result.push(arr.slice(i, i + chunkSize));
+    }
+    return result;
+  }
 
   roundDecimal(figure: number) {
     const roundedNumber = Math.ceil(figure * 10) / 10;
