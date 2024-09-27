51cea98c7c7289841569472366e7b78274885ae4
Miguel Ángel <txrz56rad@live.com.mx>
2024-09-27 04:00:32 -0600

    Integrate Material Angular

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { LoginComponent } from '../credentials/login/login.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, MatTooltipModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
@@ -12,7 +13,6 @@ export class NavbarComponent {
   isCredentialOpen = signal(true);
 
   onCredentials() {
-    // console.log('hello login btn');
     this.isCredentialOpen.set(true);
   }
 
